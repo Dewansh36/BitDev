c2bf26ebb8f735a9cd5198b4efab13378c6a157e
aman agrawal <btech10215.20@bitmesra.ac.in>
2022-01-20 00:01:24 +0530

    navbar and footer added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,27 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 import { BrowserRouter as Router, Routes, Route }
   from 'react-router-dom';
 import Home from './Components/Home/Home';
 import CP from './Components/Cp/Cp'
 import Login from './Components/Login/Login';
 import Register from './Components/Login/Registration';
+import Navbar from './Components/navbar/navbar'
+import Footer from './Components/footer/footer'
 
 function App() {
   return (
-    <Router>
+    <Router> 
+      <Navbar />
       <Routes>
         <Route exact path='/' element={<Home />} />
         <Route path='/cp' element={<CP />} />
         <Route path='/login' element={<Login />} />
-        <Route path='/registration' element={<Register />} />
-
-      </Routes>
+        <Route path='/registration' element={<Register />} /> 
+        {/* <Route path='/navbar' element={<Navbar />} />
+        <Route path='/footer' element={<Footer />} /> */}
+      </Routes> 
+      <Footer />
     </Router>
   );
 }
