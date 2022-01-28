b66a77bae1bcbbe8a6030d615d7678367b294b0b
Ankitaayush <83910445+Ankitaayush@users.noreply.github.com>
2022-01-28 15:26:57 +0530

    Added home page

--- frontend/src/Components/Home/Home.js
+++ frontend/src/Components/Home/Home.js
@@ -1,61 +1,66 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios';
-// import '../../Public/css/home.css'
-import logo from '../../Public/image/Picture1.png'
-import back from '../../Public/image/background.jpg'
+import logo from './logo.svg';
+import '../../Public/css/animation.css';
+import '../../Public/css/home.css';
+import ParticleBackground from './Particlebackground'
+import Header from "./Header";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import{Grid,Row,Col, Container}  from "react-bootstrap"
+import './animation.css'
 
-function Home() {
-    return (
-        <div>
-            <div class="pace  pace-inactive">
-                <div class="pace-progress" data-progress-text="100%" data-progress="99" style="width: 100%;">
-                    <div class="pace-progress-inner"></div>
-                </div>
-                <div class="pace-activity"></div>
-            </div>
-            <div class="top-left z-index-1 padding-30">
-                <img src={logo} width="350" height="175" data-src-retina="assets/image/logo_2x.png" alt="" />
-            </div>
-            <div class="top-right z-index-10 padding-40">
-                <a href="/register"><button class="btn btn-success btn-rounded pull-right mx-auto">Signup</button></a>
-                <a href="/login"><button class="btn text-success btn-rounded hover-shadow ripple shadow-lg "
-                    style="position: relative; right: 10px;">Login </button></a>
-            </div>
-            <section class="jumbotron full-height relative z-index-0" data-pages-bg-image={back}
-                data-bg-overlay="black" data-overlay-opacity="0.4"
-                style="background-image: url(&quot;/image/background.jpg&quot;);">
-                <div class="container-xs-height full-height">
-                    <div class="col-xs-height col-middle text-left">
-                        <div class="content-layer">
-                            <div class="inner full-height">
-                                <div class="container-xs-height full-height">
-                                    <div class="col-xs-height col-middle text-center">
-                                        <div class="container">
-                                            <div style="top: 180px;position: relative;">
-                                                <div class="col-md-6 col-md-offset-3">
-                                                    <h1 class="light text-white m-t-5 demo_relative_heading sm-no-padding hidden-xs"
-                                                        data-swiper-parallax="-15%">
-                                                        <span class="font-montserrat">BitDev</span><br />
-                                                        tagline here
-                                                    </h1>
-                                                    <h2 class="light text-white m-t-5 demo_relative_heading sm-no-padding visible-xs"
-                                                        data-swiper-parallax="-15%">
-                                                        <span class="font-montserrat ">BitDev</span><br />
-                                                        tagline here
-                                                    </h2>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="bg-overlay" style="background-color: rgb(255, 255, 255); opacity: 0.4;"></div>
-            </section>
-        </div>
-    )
+function App() {
+  return (
+    <div className="App">
+     
+     <Header/>
+     <div class="deconstructed">
+  DECONSTRUCTED
+  <div>tag line here</div>
+  <div>tag line here</div>
+  <div>tag line here</div>
+  <div>tag line here</div>
+</div>
+        
+<h1 className="title">BITDEV</h1>
+     <ParticleBackground></ParticleBackground>
+
+
+
+     <section className="text-center">
+    <Container>
+     <Row>
+       <Col lg={4}>
+            <img alt="" className="m-b-20" width="200" height="100" src="/uploadlogo.png">
+              </img>
+            <h6 className="block-title p-b-5">Launch Design <i className="pg-arrow_right"></i></h6>
+            <h1>    </h1>
+            <h1>      </h1>
+            
+
+            <p className="m-b-30">awesome project for sharing website to showoff your coding skills and get to know about
+              current project trends</p>
+              </Col>
+          
+              <Col lg={4}>
+            <img alt="" className="m-b-20" width="200" height="100" src="/teamlogofinal.jpg"></img>
+            <h6 className="block-title p-b-5">easy interaction<i className="pg-arrow_right"></i></h6>
+            <p className="m-b-30">add your college mates in your friend lists and interact with them easily</p>
+            </Col>
+
+            <Col lg={4} >
+          
+            <img alt="" className="m-b-20" width="200" height="100" src="/codeforces logo.png"></img>
+            <h6 className="block-title p-b-5">competitive programming<i className="pg-arrow_right"></i></h6>
+            <p className="m-b-30">competitive programming made easier with better interactions and uptodate competition
+              updates</p>
+              </Col>
+              
+   </Row>
+          
+      </Container>
+    
+  </section>
+    </div>
+  );
 }
 
-export default Home
+export default App;
