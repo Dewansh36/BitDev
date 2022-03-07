6ac042cd73919afaf72763a04eb04d8a68940446
Hriday Garg <hriday.1508.garg@gmail.com>
2022-03-07 21:44:56 +0530

    post and vavbar css fixed

--- frontend/src/Components/navbar/navbar.jsx
+++ frontend/src/Components/navbar/navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js'
 import logo from '../../Public/image/Picture1.png'
-import Styles from '../../Public/css/navbar.module.css'
+import '../../Public/css/navbar.css'
 
 
 const Navbar=(props) => {
@@ -11,38 +11,38 @@ const Navbar=(props) => {
     console.log(profileLink);
     return (
         <>
-            <nav className={`${Styles.navbar} ${Styles["navbar-expand-lg"]} ${Styles["navbar-light"]} ${Styles["bg-white"]} ${Styles["text-black"]} ${Styles.boxshadow}`}>
-                <div className={Styles["container-fluid"]}>
+            <nav className="navbar navbar-expand-lg navbar-light bg-white text-black Navbarboxshadow">
+                <div className="container-fluid">
                     <div>
-                        <img src={logo} className={Styles.toplogo} />
+                        <img src={logo} className="Navbartoplogo" />
                     </div>
-                    <a className={`${Styles["navbar-brand"]} ${Styles["fw-bold"]} ${Styles["text-black"]}`} id={Styles.tlogo} href="#">BitDev</a>
-                    <button className={Styles["navbar-toggler"]} type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className={Styles["navbar-toggler-icon"]}></span>
+                    <a className="navbar-brand fw-bold text-black" id="Navbartlogo" href="#">BitDev</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className={`${Styles.collapse} ${Styles["navbar-collapse"]}`} id={Styles.navbarSupportedContent}>
-                        <ul className={`${Styles["navbar-nav"]} ${Styles["me-auto"]} ${Styles["mb-2"]} ${Styles["mb-lg-0"]}`}>
-                            <li className={Styles["nav-item"]}>
-                                <a className={Styles["nav-link"]} aria-current="page" href="/project">Project</a>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link" aria-current="page" href="/project">Project</a>
                             </li>
-                            <li className={Styles["nav-item"]}>
-                                <a className={Styles["nav-link"]} href="/cp">Competitive Programming</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/cp">Competitive Programming</a>
                             </li>
                         </ul>
-                        <form className={Styles["d-flex"]} action="/search">
-                            <input name="search" className={`${Styles["form-control"]} ${Styles["me-2"]}`} type="search" placeholder="Search" aria-label="Search" />
-                            <button className={`${Styles.btn} ${Styles["btn-outline-success"]} ${Styles["me-3"]}`} type="submit">Search</button>
+                        <form className="d-flex" action="/search">
+                            <input name="search" className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                            <button className="btn btn-outline-success me-3" type="submit">Search</button>
                         </form>
-                        <ul className={`${Styles.aboutus} ${Styles["navbar-nav"]} ${Styles["ms-md-3"]}`}>
-                            <li className={Styles["nav-item"]}>
-                                <a className={Styles["nav-link"]} href="/logout">Logout <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className={`${Styles.bi} ${Styles["bi-power"]}`} viewBox="0 0 16 16">
+                        <ul className="aboutus navbar-nav ms-md-3">
+                            <li className="nav-item">
+                                <a className="nav-link" href="/logout">Logout <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-power" viewBox="0 0 16 16">
                                     <path d="M7.5 1v7h1V1h-1z" />
                                     <path d="M3 8.812a4.999 4.999 0 0 1 2.578-4.375l-.485-.874A6 6 0 1 0 11 3.616l-.501.865A5 5 0 1 1 3 8.812z" />
                                 </svg></a>
                             </li>
                         </ul>
-                        <form className={Styles["d-flex"]} action={profileLink}>
-                            <button type="submit" className={`${Styles.btn} ${Styles["btn-success"]} ${Styles["text-black"]} ${Styles["rounded-circle"]} ${Styles["mx-1"]}`}>AA
+                        <form className="d-flex" action={profileLink}>
+                            <button type="submit" className="btn btn-success text-black rounded-circle mx-1">AA
                             </button>
                         </form>
                     </div>
--- frontend/src/Components/post/Post.js
+++ frontend/src/Components/post/Post.js
@@ -1,67 +1,55 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Styles from '../../Public/css/post.module.css'
+import '../../Public/css/post.css'
 
 function Post() {
   return <div>
-        <div className={Styles.background}>
-  <div className={Styles.container}>
-    <div className={Styles.screen}>
-      {/* <div className="screen-header">
-        <div className="screen-header-left">
-          <div className="screen-header-button close"></div>
-          <div className="screen-header-button maximize"></div>
-          <div className="screen-header-button minimize"></div>
-        </div>
-        <div className="screen-header-right">
-          <div className="screen-header-ellipsis"></div>
-          <div className="screen-header-ellipsis"></div>
-          <div className="screen-header-ellipsis"></div>
-        </div>
-      </div> */}
-      <div className={Styles["screen-body"]}>
-        <div className={`${Styles.left} ${Styles["screen-body-item"]}`}>
-          <div className={Styles["app-title"]}>
-            <span>CREATE</span>
-            <span>POST</span>
+  <div className="PostBody">
+  <div className="PostBackground">
+    <div className="Postcontainer">
+      <div className="Postscreen">
+        <div className="Postscreen-body">
+          <div className="Postleft Postscreen-body-item">
+            <div className="Postapp-title">
+              <span>CREATE</span>
+              <span>POST</span>
+            </div>
+            
+            <br />
+            <div className="Postgif" style={{paddingTop: "2rem"}}>
+                <img src="https://i.pinimg.com/originals/e4/d3/95/e4d395849317f98f2a418c0e10182b0d.gif" alt="" 
+                style={{width: "230px"}} />
+            </div>
           </div>
-          
-          <br />
-          <div className={Styles.gif} style={{paddingTop: "2rem"}}>
-              <img src="https://i.pinimg.com/originals/e4/d3/95/e4d395849317f98f2a418c0e10182b0d.gif" alt="" 
-              style={{width: "230px"}} />
+          <div className="Postscreen-body-item">
+            <form className="Postapp-form">
+              <div className="Postapp-form-group">
+                <input className="PostInput Postapp-form-control" placeholder="NAME" value="Parthiv Sarkar" />
+              </div>
+              <div className="Postapp-form-group">
+                <input className="PostInput app-form-control" placeholder="Post-Name" required/>
+              </div>
+              <div className="Postmessage Postapp-form-group">
+                <textarea className="Postapp-form-control" placeholder="Post-Description"/>
+              </div>
+              <div className="Postapp-form-group">
+                <input className="PostInput Postapp-form-control" placeholder="Tech-Stack"/>
+              </div>
+              <div className="Postapp-form-group">
+                  <input className="PostInput Postapp-form-control" type="file" multiple />
+              </div>
+              <div className="Postbuttons Postapp=form-group">
+                <button className="PostButton Postapp-form-button">CANCEL</button>
+                <button className="PostButton Postapp-form-button">SEND</button>
+              </div>
+            </form>
           </div>
-        </div>
-        <div className={Styles["screen-body-item"]}>
-          <form className={Styles["app-form"]}>
-            <div className={Styles["app-form-group"]}>
-              <input className={Styles["app-form-control"]} placeholder="NAME" value="Parthiv Sarkar" />
-            </div>
-            <div className={Styles["app-form-group"]}>
-              <input className={Styles["app-form-control"]} placeholder="Post-Name" required/>
-            </div>
-            <div className={`${Styles.message} ${Styles["app-form-group"]}`}>
-              <textarea className={Styles["app-form-control"]} placeholder="Post-Description"/>
-            </div>
-            <div className={Styles["app-form-group"]}>
-              <input className={Styles["app-form-control"]} placeholder="Tech-Stack"/>
-            </div>
-            <div className={Styles["app-form-group"]}>
-                <input className={Styles["app-form-control"]} type="file" multiple />
-            </div>
-            <div className={`${Styles.buttons} ${Styles["app=form-group"]}`}>
-              <button className={Styles["app-form-button"]}>CANCEL</button>
-              <button className={Styles["app-form-button"]}>SEND</button>
-            </div>
-          </form>
         </div>
       </div>
     </div>
-    
   </div>
-</div>
-
+  </div> 
   </div>;
 }
 
