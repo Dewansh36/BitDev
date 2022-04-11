50f04b33da3f116f7035fdb2753676a0cafd90d1
Hriday Garg <hriday.1508.garg@gmail.com>
2022-04-11 20:05:59 +0530

    fixed home page

--- frontend/src/Components/Home/Home.js
+++ frontend/src/Components/Home/Home.js
@@ -2,6 +2,10 @@ import ParticleBackground from './Particlebackground'
 import Header from "./Header";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../Public/css/home.css'
+import Home1 from '../../Public/image/home1.jpg'
+import Home2 from '../../Public/image/home2.png'
+import Home3 from '../../Public/image/home3.jpg'
+
 import { Grid, Row, Col, Container } from "react-bootstrap"
 function App() {
   return (
@@ -15,34 +19,26 @@ function App() {
       <section className="home-text-center">
         <Container>
           <Row>
-            <Col lg={4}>
-              <img alt="" className="home-m-b-20" width="200" height="100" src="/uploadlogo.png">
-              </img>
-              <h6 className="home-block-title p-b-5">Launch Design <i className="pg-arrow_right"></i></h6>
-              <h1>    </h1>
-              <h1>      </h1>
-
-
-              <p className="home-m-b-30">awesome project for sharing website to showoff your coding skills and get to know about
+            <Col lg={4} className="HomeFix">
+              <img alt="" className="home-m-b-20" width="150" height="100" src={Home1}></img>
+              <h6 className="home-block-title p-b-5">Launch Design<i className="pg-arrow_right"></i></h6>
+              <p className="home-m-b-30">Awesome project for sharing website to showoff your coding skills and get to know about
                 current project trends</p>
             </Col>
 
-            <Col lg={4}>
-              <img alt="" className="home-m-b-20" width="200" height="100" src="/teamlogofinal.jpg"></img>
-              <h6 className="home-block-title p-b-5">easy interaction<i className="pg-arrow_right"></i></h6>
-              <p className="home-m-b-30">add your college mates in your friend lists and interact with them easily</p>
+            <Col lg={4} className="HomeFix">
+              <img alt="" className="home-m-b-20" width="150" height="100" src={Home2}></img>
+              <h6 className="home-block-title p-b-5">Easy Interaction<i className="pg-arrow_right"></i></h6>
+              <p className="home-m-b-30">Add your college mates in your friend lists and interact with them easily</p>
             </Col>
 
-            <Col lg={4} >
-
-              <img alt="" className="home-m-b-20" width="200" height="100" src="/codeforces logo.png"></img>
-              <h6 className="home-block-title p-b-5">competitive programming<i className="pg-arrow_right"></i></h6>
-              <p className="home-m-b-30">competitive programming made easier with better interactions and uptodate competition
+            <Col lg={4} className="HomeFix">
+              <img alt="" className="home-m-b-20" width="150" height="100" src={Home3}></img>
+              <h6 className="home-block-title p-b-5 Homecpfix">Competitive Programming<i className="pg-arrow_right"></i></h6>
+              <p className="home-m-b-30">Competitive programming made easier with better interactions and uptodate competition
                 updates</p>
             </Col>
-
           </Row>
-
         </Container>
 
       </section>
